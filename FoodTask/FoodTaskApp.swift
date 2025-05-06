c6e10713e8166903e34808a0bb3af7c71be41d68
Reza Sharifi <reza.sharifi76@gmail.com>
2025-05-06 10:25:24 +0330

    Initial Commit

---
+++ FoodTask/FoodTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodTaskApp.swift
+//  FoodTask
+//
+//  Created by Reza Sharifi on 5/6/25.
+//
+
+import SwiftUI
+
+@main
+struct FoodTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
